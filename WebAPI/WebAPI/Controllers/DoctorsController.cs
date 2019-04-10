734f06f488a7ac42b42ade7e70a4503f5ccf63c7
Iftekhar Jamil <bsse0802@iit.du.ac.bd>
2019-04-10 14:40:08 +0600

    Asp.net web api project added

---
+++ WebAPI/WebAPI/Controllers/DoctorsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class DoctorsController : ApiController
+    {
+        private TestHMSEntities db = new TestHMSEntities();
+            
+        // GET: api/Doctors
+        public IQueryable<Doctor> GetDoctors()
+        {
+            return db.Doctors;
+        }
+
+        // GET: api/Doctors/5
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult GetDoctor(int id)
+        {
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(doctor);
+        }
+
+        // PUT: api/Doctors/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDoctor(int id, Doctor doctor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != doctor.DocotorID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(doctor).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DoctorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Doctors
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult PostDoctor(Doctor doctor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Doctors.Add(doctor);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = doctor.DocotorID }, doctor);
+        }
+
+        // DELETE: api/Doctors/5
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult DeleteDoctor(int id)
+        {
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            db.Doctors.Remove(doctor);
+            db.SaveChanges();
+
+            return Ok(doctor);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DoctorExists(int id)
+        {
+            return db.Doctors.Count(e => e.DocotorID == id) > 0;
+        }
+    }
+}
